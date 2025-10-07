8ea0f0af29dde66abd8cb28d5285c465ac82bf80
Borromeu Jr <borromeujunior@hotmail.com>
2025-10-07 23:57:53 +0000

    IF Else

---
+++ primeiro exercico If_Else.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main (){
+
+/*
+if (condicao) {
+    // bloco de código a ser executado se a condicao for verdadeira
+}else {
+    // bloco de código a ser executado se a condicao for falsa
+}
+*/
+
+    
+
+
+
+}
