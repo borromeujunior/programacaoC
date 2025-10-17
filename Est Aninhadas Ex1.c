0bb873589af85add4d049169d0b33038ca2ab5cf
Borromeu Jr <borromeujunior@hotmail.com>
2025-10-17 15:44:26 +0000

    Ex1

--- Est Aninhadas Ex1.c
+++ Est Aninhadas Ex1.c
@@ -1,11 +1,26 @@
 #include <stdio.h>
 
 int main() {
+    int idade;
+    float renda;
+
+    printf("Digite sua idade: \n");
+    scanf("%d", &idade);
+    printf("Digite sua renda: \n");
+    scanf("%f", &renda);
+
+    if(idade <= 18 || idade >= 60)
+    {
+    printf("Aceito com relação à idade! \n");
+    } else {
+    printf("Você não atende aos critérios devido sua idade! \n");    
+    }
+    if(renda < 2000)
+    {
+    printf("Você tem direito ao desconto! \n");    
+    } else {
+    printf("Você não tem direito ao desconto devido à sua renda! \n");    
+    }
 
 
-
-
-
-
-    
 }
